21ec5e2a168248142413775cd14fa200519f9975
koolay <koolay@163.com>
2016-09-18 00:15:51 +0800

    static serve

--- admin/web.go
+++ admin/web.go
@@ -1,8 +1,14 @@
 package admin
 
 import (
+	"bytes"
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
 
+	"github.com/kelseyhightower/confd/log"
 	"github.com/kelseyhightower/confd/resource/template"
 
 	"github.com/kataras/iris"
@@ -27,6 +33,20 @@ func (w *WebServer) Start() {
 	//app.Favicon("./favicon.ico")
 	view := &View{WebServer: w}
 	app.Get("/", view.Home)
+
+	//service static file
+	app.Get("/static/*file", func(ctx *iris.Context) {
+		requestpath := filepath.Join("static/", ctx.Param("file"))
+		log.Debug("static path:" + requestpath)
+		path := strings.Replace(requestpath, "/", string(os.PathSeparator), -1)
+		data, err := Asset(path)
+		if err != nil {
+			ctx.NotFound()
+			return
+		}
+
+		ctx.ServeContent(bytes.NewReader(data), path, time.Now(), true)
+	})
 
 	app.Get("/api/projects", view.GetProjects)
 	app.Get("/api/project/:projectName", view.GetProject)
