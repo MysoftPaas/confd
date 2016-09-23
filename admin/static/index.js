1ab389c6bcab69d0032303d2a587855d97a54d66
koolay <koolay@163.com>
2016-09-23 18:42:13 +0800

    index

--- admin/static/index.js
+++ admin/static/index.js
@@ -2,19 +2,19 @@
     'use strict'
     exports.app = function(elId) {
         return new Vue({
-            paths: {
-                get: config.apiHost + '/api/projects'
-            },
             el: '#' + elId,
             data: {
                 items: [],
+                currentProject: {
+                    project: null,
+                    resources: []
+                },
             },
             methods: {
                 search: function() {},
                 loadData: function() {
                     var self = this
-                    var path = self.$options.paths.get;
-                    axios.get(path)
+                    axios.get(config.apiHost + '/api/projects')
                         .then(function(response) {
                             ui.hideLoading()
                             if (response.data.result === false) {
@@ -27,7 +27,23 @@
                             ui.alert('出错', '服务器端错误', 'error')
                             console.log(err)
                         })
+                },
+                selectProject: function(proj) {
+                    var self = this;
+                    self.currentProject.project = proj
+                    axios.get(config.apiHost + '/api/project/' + proj.Name)
+                        .then(function(response) {
+                            console.log(response.data);
+                            ui.hideLoading();
+                            self.currentProject.resources = response.data.resources;
+
+                        }).catch(function(err) {
+                            ui.hideLoading()
+                            ui.alert('出错', '服务器端错误', 'error')
+                        })
+
                 }
+
             },
             filters: {
                 //moment: function(timestamp) {
